a8fa43edbaa9abe0792e2f97c49761bebea10d0a
h-shiraishi0402 <hiro19900402@gmail.com>
2021-01-07 16:01:37 +0900

    first commit

--- Youtube/Controller/AppDelegate.swift
+++ Youtube/Controller/AppDelegate.swift
@@ -10,12 +10,74 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
+    let hours = 19
+    let minunte = 00
+    
+    
+    var notifacationGranted = true
+    
+    var isFirst = true
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //許可を促すアラートを出す
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound]) { (grantid, error) in
+        
+            
+            self.notifacationGranted = grantid
+            if let error = error{
+                print(error)
+                
+            }
+        }
+        
+        isFirst = false
+        setNotifacation()
+        
         return true
     }
+    
+    
+    func setNotifacation(){
+        var notificationTime = DateComponents()
+        var trigger:UNNotificationTrigger
+        
+        notificationTime.hour = hours
+        notificationTime.minute = minunte
+        trigger = UNCalendarNotificationTrigger(dateMatching: notificationTime, repeats: true)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "19jininarimasita"
+        content.body = "動画が更新されました"
+        content.sound = .default
+        
+        //通知スタイル
+        let reqest = UNNotificationRequest(identifier: "uuid", content: content, trigger: trigger)
+        
+        //通知をセットする
+        UNUserNotificationCenter.current().add(reqest, withCompletionHandler: nil)
+        
+        
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        setNotifacation()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     // MARK: UISceneSession Lifecycle
 
--- Youtube/Model/YoutubeData.swift
+++ Youtube/Model/YoutubeData.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+class YoutubeData{
+    
+    var videoId:String = ""
+    var publishedAt:String = ""
+    var title:String = ""
+    var imageURLString:String = ""
+    var youtubeURL:String = ""
+    var channelTitle:String = ""
+    
+    
+    
+    init() {
+        
+    }
+    
+    
+    
+    
+    
+    
+}
